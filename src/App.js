221aa3abe02abf8f39e7a67c231da63b48e21cec
unknown <3334976@gmail.com>
2021-12-01 11:56:32 +0300

    0112 1156

--- src/App.js
+++ src/App.js
@@ -1,19 +1,48 @@
+import { Component } from 'react';
+
 import './App.css';
 
-function WhoAmI ({name, surname, link}) {
-  return (
-    <div>
-      <h1>My name is {name()}, surmame - {surname}</h1>
-      <a href={link}>My profile</a>
-    </div>
-  );
+class WhoAmI extends Component {
+  constructor(props) {
+    super(props);
+    // создаем состояние компонента, которое можно изменить динамически
+    this.state = {
+      years: 27,
+      text: '+++',
+    };
+  }
+
+  nextYear = () => {
+    console.log('+++');
+    // устанавливаем новое состояние с помощью метода setState
+    // внутри устанавливаем колбэк ф-цию которая возвращает
+    // значиние объекта, которое увеличивается на 1 при нажатии на кнопку (колбэк ф-ция
+    //нужна для того, чтобы новое значение устанавливалось только после нажатия на кнопку)
+    this.setState((state) => ({
+      years: state.years + 1,
+    }));
+  };
+
+  render() {
+    const { name, surname, link } = this.props;
+    return (
+      <div>
+        {/* Обработчик события на клик */}
+        <button onClick={this.nextYear}>{this.state.text}</button>
+        <h1>
+          My name is {name}, surmame - {surname}, age - {this.state.years}
+        </h1>
+        <a href={link}>My profile</a>
+      </div>
+    );
+  }
 }
 
 function App() {
   return (
     <div className="App">
-      <WhoAmI name={() => {return 'John'}} surname="Smith" link="facebook.com"/>
-      <WhoAmI name={() => {return 'John'}} surname="Shepard" link="vk.com"/>
+      <WhoAmI name={'John'} surname="Smith" link="facebook.com" />
+      <WhoAmI name={'Alex'} surname="Shepard" link="vk.com" />
     </div>
   );
 }
