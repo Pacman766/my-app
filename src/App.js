5bf7d9346d4d0e212b3e80ab2d546a72704f2c9d
unknown <3334976@gmail.com>
2022-02-15 19:33:25 +0300

    useEffect hook practice

--- src/App.js
+++ src/App.js
@@ -1,90 +1,48 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import { useRef, useState, useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 import './App.css';
 
-class Form extends Component {
-  state = {
-    advOpen: false, // state for portal
-  };
+const Form = () => {
+  const [text, setText] = useState('');
 
-  // opening portal after 3sec
-  componentDidMount() {
-    setTimeout(this.handleClick, 3000);
-  }
+  const myRef = useRef(1);
 
-  // setting opposit state for portal
-  handleClick = () => {
-    this.setState(({ advOpen }) => ({
-      advOpen: !advOpen,
-    }));
-  };
+  // const focusFirstTI = () => {
+  //   myRef.current.focus();
+  // };
 
-  render() {
-    return (
-      <Container>
-        <form
-          // open and close portal on click
-          onClick={this.handleClick}
-          className="w-50 border mt-5 p-3 m-auto"
-          style={{ overflow: 'hidden', position: 'relative' }}
-        >
-          <div className="mb-3">
-            <label htmlFor="exampleFormControlInput1" className="form-label">
-              Email address
-            </label>
-            <input
-              type="email"
-              className="form-control"
-              id="exampleFormControlInput1"
-              placeholder="name@example.com"
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="exampleFormControlTextarea1" className="form-label">
-              Example textarea
-            </label>
-            <textarea
-              className="form-control"
-              id="exampleFormControlTextarea1"
-              rows="3"
-            ></textarea>
-          </div>
-          {/* if advOpen - clicked - show portal */}
-          {this.state.advOpen ? (
-            <Portal>
-              <Msg />
-            </Portal>
-          ) : null}
-        </form>
-      </Container>
-    );
-  }
-}
+  useEffect(() => {
+    myRef.current = text;
+  });
 
-// create portal
-const Portal = (props) => {
-  const node = document.createElement('div');
-  document.body.appendChild(node);
-
-  return ReactDOM.createPortal(props.children, node);
-};
-
-// create msg in portal with styles
-const Msg = () => {
   return (
-    <div
-      style={{
-        width: '500px',
-        height: '150px',
-        backgroundColor: 'red',
-        position: 'absolute',
-        right: '0',
-        bottom: '0',
-      }}
-    >
-      Hello
-    </div>
+    <Container>
+      <form className="w-50 border mt-5 p-3 m-auto">
+        <div className="mb-3">
+          <label htmlFor="exampleFormControlInput1" className="form-label">
+            Email address
+          </label>
+          <input
+            onChange={(e) => setText(e.target.value)}
+            type="email"
+            className="form-control"
+            id="exampleFormControlInput1"
+            placeholder="name@example.com"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleFormControlTextarea1" className="form-label">
+            Example textarea
+          </label>
+          <textarea
+            value={myRef.current}
+            className="form-control"
+            id="exampleFormControlTextarea1"
+            rows="3"
+          ></textarea>
+        </div>
+      </form>
+    </Container>
   );
 };
 
